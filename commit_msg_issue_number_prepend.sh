0974ea6e8ca09d313ab7d4dac8d7a98e5394d133
Seth Paul <sethcpaul@gmail.com>
2024-10-31 10:45:52 -0600

    #16 enrich with cross repo

--- commit_msg_issue_number_prepend.sh
+++ commit_msg_issue_number_prepend.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-ISSUE_NUMBER=$(git symbolic-ref HEAD | sed -nE 's/[A-Za-z//-_]*([0-9]+).*/\1/p')
+ISSUE_NUMBER=$(git symbolic-ref HEAD --short | sed -nE 's/[A-Za-z\-_]*([0-9]+).*/\1/p')
+REPO_NAME=$(git symbolic-ref HEAD --short | sed -nE 's/([A-Za-z]*)[\-_]*[0-9]+.*/\1/p')
+REPO_PREPEND=""
+if [ "$REPO_NAME" = "gh" ]; then
+    REPO_PREPEND=""
+fi
+
+if [ "$REPO_NAME" = "nimbee" ]; then
+    REPO_PREPEND="hipponot/nimbee"
+fi
+if [ "$REPO_NAME" = "vega" ]; then
+    REPO_PREPEND="hipponot/vega"
+fi
+if [ "$REPO_NAME" = "ms" ]; then
+    REPO_PREPEND="hipponot/microservices"
+fi
+if [ "$REPO_NAME" = "scs" ]; then
+    REPO_PREPEND="saga-ed/saga-cu-shared"
+fi
+
+
 if [ -n "$ISSUE_NUMBER" ]; then
-    echo "#$ISSUE_NUMBER" `cat $1`  > "$1"
+    echo "$REPO_PREPEND#$ISSUE_NUMBER" `cat $1`  > "$1"
 fi
