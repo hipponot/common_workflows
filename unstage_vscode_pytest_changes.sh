1824a377172f8944e5cdb78eb538bb7e6546f6ed
Seth Paul <sethcpaul@gmail.com>
2025-01-22 13:03:39 -0700

    iterate messages

--- unstage_vscode_pytest_changes.sh
+++ unstage_vscode_pytest_changes.sh
@@ -8,9 +8,10 @@ fi
 
 # Find files to be committed
 (
-IFS='
-'
-SETTINGS=$(git diff-index --cached $against --name-only | grep -a '\.vscode\/settings\.json$' | uniq)
+# IFS='
+# '
+# SETTINGS=$(git diff-index --cached $against --name-only | grep -a '\.vscode\/settings\.json$' | uniq)
+mapfile -t SETTINGS < <(git diff-index --cached "$against" --name-only | grep -a '\.vscode\/settings\.json$' | uniq)
 if [ -z "$SETTINGS" ]; then
     exit 0
 fi
@@ -18,9 +19,8 @@ echo "Found settings files: $SETTINGS"
 for SETTING in $SETTINGS ; do
     echo "--- Checking $SETTING"
     Settings_change=$(git diff --cached --unified=0 "$SETTING" | grep -Po '(?<=^\+)(?!\+\+).*')
-    echo "------ Settings change to pytestEnabled: $Settings_change"
-   if echo "$Settings_change" | jq -e '.["python.testing.pytestEnabled"] == true' ; then
-      echo "------ pytestEnabled: true found in $SETTING"
+   if echo "$Settings_change" | grep -q 'pytestEnabled": true' ; then
+      echo "------ python.testing.pytestEnabled: true found in $SETTING"
       echo -e "\033[33m------ Unstaging $SETTING. If this is not what you want, run commit with SKIP=stage_vscode_pytest_changes git commit ...\033[0m"
       git reset "$SETTING"
    fi
